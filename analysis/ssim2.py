91c8f21c95d4fb86a033ae8177468e88e75b8b71
Peter <pjb45@duke.edu>
2024-11-20 16:29:45 -0500

    put everything in ssim2 and added an eval ipynb.

--- analysis/ssim2.py
+++ analysis/ssim2.py
@@ -3,6 +3,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 # SSIM meaning
 # 1 is perfect similarity
@@ -91,10 +92,77 @@ def ssim_score(image_real_out_path,
 
     return ssim, luminance, contrast, structure
 
+#### HOW TO USE ##################################################
+
 # Three images are real_pair (real_in, real_out) and fake_out
 image_real_out_path = 'data/frame_0_real.png'
 image_fake_out_path = 'data/frame_0_fake.png'
 
 ssim, luminance, contrast, structure = ssim_score(image_real_out_path, image_fake_out_path, plotOn=True)
+
+####################################################################################################
+####################################################################################################
+
+
+## TODO:: NEED TO INCLUDE FUNCTIONALITY ABOUT THE WINDOW SLIDING AND AVERAGING
+
+def evaluate_folder_ssim(folder_path, 
+                    WINDOW_SIZE=None,
+                    ALPHA_chosen=None,BETA_chosen=None, GAMMA_chosen=None,
+                    plotAll=False,
+                    plotScores=False):
+
+    #### Designate Image Source
+
+    # find all images with a name containing 'real'
+    real_images = [fileName for fileName in os.listdir(folder_path) if 'real' in fileName]
+    real_images.sort()
+
+    # find all images with a name containing 'cycle'
+    cycle_images = [fileName for fileName in os.listdir(folder_path) if 'cycle' in fileName]
+    cycle_images.sort()
+
+    # find all images with a name containing 'pix2pix'
+    pix2pix_images = [fileName for fileName in os.listdir(folder_path) if 'pix2pix' in fileName]
+    pix2pix_images.sort()
+
+    # find all images with a name containing 'ground_truth'
+    ground_truth_images = [fileName for fileName in os.listdir(folder_path) if 'ground_truth' in fileName]
+
+    ##### Get the SSIM scores for each image
+    ssim_scores_pix2pix = []
+    ssim_scores_cycle = []
+    
+    for i in range(len(real_images)):
+        image_number = real_images[i].split('_')[0]
+
+        ssim_scores_pix2pix.append(ssim2.ssim_score(os.path.join(folder_path, real_images[i]), 
+                                                    os.path.join(folder_path, pix2pix_images[i]),
+                                                    WINDOW_SIZE=WINDOW_SIZE,
+                                                    ALPHA_chosen=ALPHA_chosen, BETA_chosen=BETA_chosen, GAMMA_chosen=GAMMA_chosen,
+                                                    plotOn=plotAll,
+                                                    image_number=image_number,
+                                                    generation_type='PIX'))
+        ssim_scores_cycle.append(ssim2.ssim_score(os.path.join(folder_path, real_images[i]),
+                                                    os.path.join(folder_path, cycle_images[i]),
+                                                    WINDOW_SIZE=WINDOW_SIZE,
+                                                    ALPHA_chosen=ALPHA_chosen, BETA_chosen=BETA_chosen, GAMMA_chosen=GAMMA_chosen,
+                                                    plotOn=plotAll,
+                                                    image_number=image_number,
+                                                    generation_type='CYC'))
+        
+    if plotScores:
+        plt.plot(ssim_scores_pix2pix, label='pix2pix')
+        plt.plot(ssim_scores_cycle, label='CycleGAN')
+        plt.legend()
+        plt.show()
+        
+    return ssim_scores_pix2pix, ssim_scores_cycle
+
+#### HOW TO USE ##################################################
+
+folder_path = 'cycle_pix_maps_compared'
+
+ssim_scores_pix2pix, ssim_scores_cycle = evaluate_folder_ssim(folder_path, plotScores=True)
 
 
